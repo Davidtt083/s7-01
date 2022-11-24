1ecdc10f6f33fb9e19b4dee9a8ce2684f8912af4
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2022-11-24 17:13:48 -0600

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const box = document.getElementById('quiz');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function handleClick() {
+
+    box.style.display = 'block';
+});
